76cbc24415ddff0f1d35100eb881235f14d5a812
fengchengang <urchankong@outlook.com>
2017-10-18 18:32:23 +0800

    完善控件，增加自定义配置项

--- library/src/main/java/com/github/chengang/library/TickView.java
+++ library/src/main/java/com/github/chengang/library/TickView.java
@@ -1,6 +1,7 @@
 package com.github.chengang.library;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -16,50 +17,47 @@ import android.view.View;
 
 public class TickView extends View {
 
+    private Context mContext;
+
+    //内圆的画笔
     private Paint mPaintCircle;
+    //外层圆环的画笔
     private Paint mPaintRing;
+    //打钩的画笔
     private Paint mPaintTick;
 
+    //整个圆外切的矩形
     private RectF mRectF;
+    //记录打钩路径的三个点坐标
+    private float[] mPoints = new float[8];
 
+    //控件中心的X,Y坐标
     private int centerX;
     private int centerY;
 
+    //计数器
     private int circleCounter = 0;
     private int scaleCounter = 45;
     private int ringCounter = 0;
-
-    private float[] mPoints = new float[8];
-
-    private boolean isChecked = false;
-    private Context mContext;
-
-    /**
-     * 勾的半径
-     */
-    private static float TICK_RADIUS = 35;
-    /**
-     * 勾的偏移
-     */
-    private static float TICK_RADIUS_OFFSET = 10;
-
-    /**
-     * 圆环进度增加的单位（小于90）
-     */
-    private static int RING_COUNTER_UNIT = 10;
-
-    /**
-     * 圆圈收缩的单位
-     */
-    private static int CIRCLE_COUNTER_UNIT = 6;
-
-    /**
-     * 圆圈最后放大收缩的单位
-     */
-    private static int SCALE_COUNTER_UNIT = 4;
-
     private int alphaCount = 0;
 
+    //是否被点亮
+    private boolean isChecked = false;
+
+    //勾的半径()
+    private static final float TICK_RADIUS = 35;
+    //勾的偏移
+    private static final float TICK_RADIUS_OFFSET = 10;
+    //圆环进度增加的单位（小于90）
+    private static final int RING_COUNTER_UNIT = 10;
+    //圆圈收缩的单位
+    private static final int CIRCLE_COUNTER_UNIT = 6;
+    //圆圈最后放大收缩的单位
+    private static final int SCALE_COUNTER_UNIT = 4;
+
+    private int unCheckBaseColor;
+    private int checkBaseColor;
+    private int checkTickColor;
 
     public TickView(Context context) {
         this(context, null);
@@ -72,26 +70,35 @@ public class TickView extends View {
     public TickView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mContext = context;
+        initAttrs(attrs);
         init();
+    }
+
+    private void initAttrs(AttributeSet attrs) {
+        TypedArray typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.TickView);
+        unCheckBaseColor = typedArray.getColor(R.styleable.TickView_uncheck_base_color, getResources().getColor(R.color.tick_gray));
+        checkBaseColor = typedArray.getColor(R.styleable.TickView_check_base_color, getResources().getColor(R.color.tick_yellow));
+        checkTickColor = typedArray.getColor(R.styleable.TickView_uncheck_base_color, getResources().getColor(R.color.tick_white));
+        typedArray.recycle();
     }
 
     private void init() {
         mPaintRing = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintRing.setStyle(Paint.Style.STROKE);
         if (isChecked) {
-            mPaintRing.setColor(mContext.getResources().getColor(R.color.xxx));
+            mPaintRing.setColor(checkBaseColor);
         } else {
-            mPaintRing.setColor(mContext.getResources().getColor(R.color.yyy));
+            mPaintRing.setColor(unCheckBaseColor);
         }
         mPaintRing.setStrokeCap(Paint.Cap.ROUND);
         mPaintRing.setStrokeWidth(8);
 
         mPaintCircle = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintCircle.setColor(mContext.getResources().getColor(R.color.xxx));
+        mPaintCircle.setColor(checkBaseColor);
         mPaintCircle.setStrokeWidth(4);
 
         mPaintTick = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mPaintTick.setColor(isChecked ? Color.WHITE : mContext.getResources().getColor(R.color.yyy));
+        mPaintTick.setColor(isChecked ? checkTickColor : unCheckBaseColor);
         mPaintTick.setStyle(Paint.Style.STROKE);
         mPaintTick.setStrokeCap(Paint.Cap.ROUND);
         mPaintTick.setStrokeWidth(8);
@@ -107,6 +114,7 @@ public class TickView extends View {
 
         mRectF.set(centerX - 80, centerY - 80, centerX + 80, centerY + 80);
 
+        //设置打钩的几个点坐标
         mPoints[0] = centerX - TICK_RADIUS + TICK_RADIUS_OFFSET;
         mPoints[1] = (float) centerY;
         mPoints[2] = centerX - TICK_RADIUS / 2 + TICK_RADIUS_OFFSET;
@@ -133,10 +141,10 @@ public class TickView extends View {
         canvas.drawArc(mRectF, 90, ringCounter, false, mPaintRing);
 
         if (ringCounter == 360) {
-            mPaintCircle.setColor(mContext.getResources().getColor(R.color.xxx));
+            mPaintCircle.setColor(checkBaseColor);
             canvas.drawCircle(centerX, centerY, 90, mPaintCircle);
             //绘制白色的圆
-            mPaintCircle.setColor(Color.WHITE);
+            mPaintCircle.setColor(checkTickColor);
             circleCounter += CIRCLE_COUNTER_UNIT;
             canvas.drawCircle(centerX, centerY, 90 - circleCounter, mPaintCircle);
             if (circleCounter >= 130) {
